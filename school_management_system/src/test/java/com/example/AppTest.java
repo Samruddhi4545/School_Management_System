402c68f26ea0f34157ba836dd84f95ec56decbab
samruddhi <paisamruddhi4545@gmail.com>
2025-11-05 10:31:39 +0530

    files

--- school_management_system/src/test/java/com/example/AppTest.java
+++ school_management_system/src/test/java/com/example/AppTest.java
@@ -1,13 +1,12 @@
 package com.example;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
